35e09b2831126c1d2aa55aacbb50fbfbb3b503a6
architecturequeen <olawunmitaylor@gmail.com>
2020-03-17 19:23:52 +0000

    feat: create lambda to delete todo item

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,33 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
   // TODO: Remove a TODO item by id
-  return undefined
+  await docClient
+    .delete({
+      TableName: todoTable,
+      Key: {
+        todoId: todoId
+      }
+    })
+    .promise()
+
+  return {
+    statusCode: 204,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: ''
+  }
 }
